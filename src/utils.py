f5d44246eed446dce57d4e4eb102e01c6c1c6bd2
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2022-12-18 21:58:19 +0900

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -12,3 +12,34 @@ def set_seed(seed: int = 42):
     torch.cuda.manual_seed(seed)
     torch.backends.cudnn.deterministic = True
     torch.backends.cudnn.benchmark = False
+
+
+class ModelCheckpoint():
+    def __init__(self, save_model_path, filename):
+        self.best_score=np.inf
+        self.save_path_filename = os.path.join(save_model_path, f'{filename}.pth')
+    
+    def __call__(self, model, current_score):
+        if current_score < self.best_score:
+            self.best_score = current_score
+            torch.save(model.state_dict(), self.save_path_filename)
+
+
+class EarlyStopping:
+    def __init__(self, patience=10, verbose=1):
+        self.epoch = 0
+        self.pre_loss = float('inf')
+        self.patience = patience
+        self.verbose = verbose
+        
+    def __call__(self, current_loss):
+        if self.pre_loss < current_loss:
+            self.epoch += 1
+            if self.epoch > self.patience:
+                if self.verbose:
+                    print('early stopping')
+                return True
+        else:
+            self.epoch = 0
+            self.pre_loss = current_loss
+        return False
